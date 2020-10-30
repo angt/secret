d35e867ccc5c2fe7411777b75aa24feeca137e06
Adrien Gallouët <adrien@gallouet.fr>
2020-10-30 14:56:51 +0000

    Bump to 0.11
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- secret.c
+++ secret.c
@@ -20,7 +20,7 @@
 
 #define S_COUNT(x)  (sizeof(x) / sizeof((x)[0]))
 #define S_VER_MAJOR  0U
-#define S_VER_MINOR  10U
+#define S_VER_MINOR  11U
 #define S_ENTRYSIZE  512U
 #define S_PWDGENLEN  25U
 #define S_KEYLENMAX  255U
